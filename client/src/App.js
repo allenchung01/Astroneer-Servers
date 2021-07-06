a952347b39d530ebf0a66c942b841d7a3e5794e8
allenchung <allenchung01@gmail.com>
2021-07-05 21:49:40 -0500

    Page now fetches server listings from database and displays them.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
 import "./ServerListing.js";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ServerListing from "./ServerListing";
+import axios from "axios";
 
 function App() {
-  const [ServerListings, SetServerListings] = useState([
-    { server_name: "Hound", owner_name: "John", ip_address: "5.62.127.170" },
-  ]);
+  const [ServerListings, SetServerListings] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/servers")
+      .then((response) => {
+        SetServerListings(response.data);
+        //console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="App">
@@ -21,7 +32,7 @@ function App() {
         </ul>
       </nav>
       {ServerListings.map((listing, index) => {
-        return <ServerListing listing={listing} id={index} />;
+        return <ServerListing listing={listing} key={index} />;
       })}
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,7 +1,35 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
 
 const app = express();
 
+const pool = new Pool({
+  host: "localhost",
+  user: "allenchung",
+  database: "astroneer_servers",
+  password: "password",
+  port: 5432,
+});
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/api/servers", (req, res) => {
+  const query = "SELECT * FROM servers;";
+  pool
+    .query(query)
+    .then((result) => {
+      res.json(result.rows);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
 app.listen(5000, () => {
-    console.log('Server listening on port 5000...')
-})
+  console.log("Server listening on port 5000...");
+});
