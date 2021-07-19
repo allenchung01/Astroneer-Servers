7d7490148003e2e70d90c50f98fa2bd3449f6e1c
allenchung <allenchung01@gmail.com>
2021-07-19 00:55:21 -0500

    A little more responsive in mobile.

--- client/src/Components/ServerView.js
+++ client/src/Components/ServerView.js
@@ -21,20 +21,68 @@ export default function ServerView(props) {
   }, [id]);
 
   return (
-    <div className="server-view">
+    <div>
       {server ? (
-        <div>
+        <div className="server-view">
           <h1>{server.server_name}</h1>
-          <div className="server-information">
-            <h2>{server.server_status ? "Online" : "Offline"}</h2>
-            <h2>{server.owner_name}</h2>
-            <h2>{server.server_url}</h2>
-            <h2>{server.server_region}</h2>
-            <h2>{server.server_type}</h2>
-            <h2>{server.server_game_mode}</h2>
-            <h2>{server.server_password}</h2>
-            <h2>{server.server_description}</h2>
-            <h2>{server.server_rules}</h2>
+          <div id="flex-box-1">
+            <div className="server-information-section">
+              <h2 id="server-info-heading">Server Info</h2>
+              <div id="column-1">
+                <div className="information-field">
+                  <h3 className="key">Status</h3>
+                  <h3 className="value">
+                    <span
+                      className={
+                        server.server_status ? "dot-green" : "dot-grey"
+                      }
+                    ></span>
+                    {server.server_status ? "Online" : "Offline"}
+                  </h3>
+                </div>
+                <div className="information-field">
+                  <h3 className="key">Owner</h3>
+                  <h3 className="value">{server.owner_name}</h3>
+                </div>
+                <div className="information-field">
+                  <h3 className="key">Server Url</h3>
+                  <h3 className="value">{server.server_url}</h3>
+                </div>
+
+                <div className="information-field">
+                  <h3 className="key">Region</h3>
+                  <h3 className="value">{server.server_region}</h3>
+                </div>
+              </div>
+              <div id="column-2">
+                <div className="information-field">
+                  <h3 className="key">Server Type</h3>
+                  <h3 className="value">{server.server_type}</h3>
+                </div>
+                <div className="information-field">
+                  <h3 className="key">Game Mode</h3>
+                  <h3 className="value">{server.server_game_mode}</h3>
+                </div>
+                <div className="information-field">
+                  <h3 className="key">Password</h3>
+                  <h3 className="value">{server.server_password}</h3>
+                </div>
+              </div>
+            </div>
+            <div id="flex-box-2">
+              <div className="server-description-section">
+                <h2>Description</h2>
+                <div className="information-field">
+                  <h3 className="value">{server.server_description}</h3>
+                </div>
+              </div>
+              <div className="server-rules-section">
+                <h2>Rules</h2>
+                <div className="information-field">
+                  <h3 className="value">{server.server_rules}</h3>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       ) : null}
